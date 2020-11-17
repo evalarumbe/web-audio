3c2ab3a2dba96355a4884c139a1df5efef5f46a5
Eva Larumbe <evalarumbe@gmail.com>
2020-11-17 14:08:33 +0100

    Allow user to record notes. So far the notes only have timing data, no sound yet.

--- hello-world.js
+++ hello-world.js
@@ -1,78 +1,148 @@
-(() => {
+try {
+  (() => {
     // create audio context
     const ctx = new AudioContext();
     // const context = new (window.AudioContext || window.webkitAudioContext)(); // TODO: if not working on Safari, try this
     
+    // Recording state
+    let recording = false;
+    let currentNote = new Note();
+
+    let recordedNotes = [];
+
+    console.log('When you pick back up next time: Hit record a bunch of times and look at the console for a refresher on what this does');
+
+    function Note(startTime = null, stopTime = null) {
+      return {
+        start: startTime,
+        stop: stopTime,
+        setStart: function(time) {
+          this.start = time;
+        },
+        setStop: function(time) {
+          this.stop = time;
+        },
+        getDuration: function() {
+          if (this.start && this.stop) {
+            return this.stop - this.start;
+          } else {
+            console.warn("\nNote start and stop times aren't initialized.\nIf this was unexpected, please check that the recording successfully started and stopped.\nYou may also need to reload the page.");
+            return 0;
+          }
+        }
+      }
+    }
     
-    const play = () => {
-        // create and configure oscillator source
-        const oscillator = ctx.createOscillator();
-        oscillator.type = 'sawtooth';
-        oscillator.frequency.value = 250;
+    function toggleRecState(time) {
+      recording = !recording;
+      console.log('recording:', recording);
 
-        // create and configure gain node
-        const gain = ctx.createGain();
-        
-        // begin managing time
-        const now = ctx.currentTime;
+      if (recording) {
+        startRecord(time);
+      } else {
+        stopRecord(time);
+      }
+    }
 
-        // manage volume
-        gain.gain.setValueAtTime(0.5, now);
-        gain.gain.exponentialRampToValueAtTime(0.03, now + 0.5)
-
-        // chain everything
-        oscillator.connect(gain);
-        gain.connect(ctx.destination);
-        oscillator.start();
+    function startRecord(time) {
+      console.log('started recording at ', time);
+      currentNote.start = time;
     };
     
-    const playBuzz = () => {
-        const now = ctx.currentTime;
+    function stopRecord(stopTime) {
+      console.log('stopped recording at ', stopTime);
+      currentNote.stop = stopTime;
 
-        // create and configure oscillator source
-        const oscillator = ctx.createOscillator();
-        oscillator.type = 'sawtooth';
-        oscillator.frequency.value = 440;
+      // save
+      recordedNotes.push(currentNote);
 
-        // create and configure gain node
-        const gain = ctx.createGain();
-        gain.gain.setValueAtTime(1, now);
-        gain.gain.exponentialRampToValueAtTime(0.001, now + 0.5)
+      // reset, ready for next note
+      currentNote = new Note();
+      
+      // verify
+      logRecordings();
+    }
 
-        // chain everything
-        oscillator.connect(gain);
-        gain.connect(ctx.destination);
+    function play() {
+      // create and configure oscillator source
+      const oscillator = ctx.createOscillator();
+      oscillator.type = 'sawtooth';
+      oscillator.frequency.value = 250;
 
-        // once everything's plugged in, turn it on
-        oscillator.start(0);
-        oscillator.stop(now + 1);
-        // oscillator.disconnect(0);
-    };
+      // create and configure gain node
+      const gain = ctx.createGain();
+      
+      // begin managing time
+      const now = ctx.currentTime;
+
+      // manage volume
+      gain.gain.setValueAtTime(0.5, now);
+      gain.gain.exponentialRampToValueAtTime(0.03, now + 0.5)
+
+      // chain everything
+      oscillator.connect(gain);
+      gain.connect(ctx.destination);
+      oscillator.start();
+    }
     
-    const playTinFuzz = () => {
-        const now = ctx.currentTime;
+    function playBuzz() {
+      const now = ctx.currentTime;
 
-        // create and configure oscillator source
-        const oscillator = ctx.createOscillator();
-        oscillator.type = 'sawtooth';
-        oscillator.frequency.value = 440;
+      // create and configure oscillator source
+      const oscillator = ctx.createOscillator();
+      oscillator.type = 'sawtooth';
+      oscillator.frequency.value = 440;
 
-        // create and configure gain node
-        const gain = ctx.createGain();
-        gain.gain.setValueAtTime(1, now);
-        gain.gain.exponentialRampToValueAtTime(0.001, now + 0.5)
+      // create and configure gain node
+      const gain = ctx.createGain();
+      gain.gain.setValueAtTime(1, now);
+      gain.gain.exponentialRampToValueAtTime(0.001, now + 0.5)
 
-        // chain everything
-        oscillator.connect(gain);
-        gain.connect(ctx.destination);
+      // chain everything
+      oscillator.connect(gain);
+      gain.connect(ctx.destination);
 
-        // once everything's plugged in, turn it on
-        oscillator.start(0);
-        oscillator.stop(now + 1);
-        // oscillator.disconnect(1);
-    };
+      // once everything's plugged in, turn it on
+      oscillator.start(0);
+      oscillator.stop(now + 1);
+      // oscillator.disconnect(0);
+    }
+    
+    function playTinFuzz() {
+      const now = ctx.currentTime;
+
+      // create and configure oscillator source
+      const oscillator = ctx.createOscillator();
+      oscillator.type = 'sawtooth';
+      oscillator.frequency.value = 440;
+
+      // create and configure gain node
+      const gain = ctx.createGain();
+      gain.gain.setValueAtTime(1, now);
+      gain.gain.exponentialRampToValueAtTime(0.001, now + 0.5)
+
+      // chain everything
+      oscillator.connect(gain);
+      gain.connect(ctx.destination);
+
+      // once everything's plugged in, turn it on
+      oscillator.start(0);
+      oscillator.stop(now + 1);
+      // oscillator.disconnect(1);
+    }
+
+    function logRecordings() {
+      console.log('\nrecordedNotes:');
+      recordedNotes.forEach(note => {
+        console.log('Duration:', note.getDuration());
+      });
+    }
 
     // Controls
+    document.querySelector('#rec').addEventListener('click', e => {
+        toggleRecState(new Date());
+    });
+
     document.querySelector('#play').addEventListener('click', () => {
         ctx.resume().then(() => {
             play();
@@ -81,27 +151,31 @@
     });
 
     document.querySelector('#stop').addEventListener('click', () => {
-        ctx.suspend().then(() => {
-            console.log('Playback suspended successfully');
-        });
+      ctx.suspend().then(() => {
+          console.log('Playback suspended successfully');
+      });
+      recording = false;
     });
 
     // Experiments
     document.querySelector('#buzz').addEventListener('click', () => {
-        ctx.resume().then(() => {
-            playBuzz();
-            console.log('Played buzz');
-        });
+      ctx.resume().then(() => {
+          playBuzz();
+          console.log('Played buzz');
+      });
     });
 
     document.querySelector('#tin-fuzz').addEventListener('click', () => {
-        ctx.resume().then(() => {
-            playTinFuzz();
-            console.log('Played tin fuzz');
-        });
+      ctx.resume().then(() => {
+          playTinFuzz();
+          console.log('Played tin fuzz');
+      });
     });
 
     // Auto-play
     // With this running in live-server you can live-update frequencies, i.e. live-code music
     // document.querySelector('#play').dispatchEvent(new MouseEvent('click'));
-})();
+  })();
+} catch (error) {
+  console.log(error);
+}
